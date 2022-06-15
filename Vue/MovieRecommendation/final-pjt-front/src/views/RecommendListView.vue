1f0654fe56eebbd98e46e4d3f8e38fa3036f53b9
sc-choi2022 <c.martin20222023@gmail.com>
2022-06-15 18:58:52 +0900

    OTT logo image

--- Vue/MovieRecommendation/final-pjt-front/src/views/RecommendListView.vue
+++ Vue/MovieRecommendation/final-pjt-front/src/views/RecommendListView.vue
@@ -3,15 +3,15 @@
     <div class="container">
       <div class="gallery1">
         <figure class="gallery_item item1"><router-link 
-          :to="{ name: 'recommendOtt', params: {ottId: 8} }"><img src="https://w.namu.la/s/984a3a524c3a76ef69967a3538f0b655d9e4a9b948314bd8d57f34a1502753104f1613f356c08d4352cdfad86bbea9cd56b2133ea5c9c7a57f1c065b1048cb5b99a3d5c985c3e5dcc9771079d20c1e9243522e2675093317ec2656221a51fa59" alt="netflix" height="200" width="300">
+          :to="{ name: 'recommendOtt', params: {ottId: 8} }"><img src="https://w.namu.la/s/984a3a524c3a76ef69967a3538f0b655d9e4a9b948314bd8d57f34a1502753104f1613f356c08d4352cdfad86bbea9cd56b2133ea5c9c7a57f1c065b1048cb5b99a3d5c985c3e5dcc9771079d20c1e92938bf03fa9d6d2f07d75fe6df97abaff" alt="netflix" height="200" width="300">
           </router-link>
         </figure>
         <figure class="gallery_item item2"><router-link 
-          :to="{ name: 'recommendOtt', params: {ottId: 97} }"><img src="https://w.namu.la/s/806795f01bfdb99ee07ef5ed4d26c7a76ce99bddf229f51418d040f4387944e4e06d19bcbd84da2a179de6273e3ca30f084aabc97154fb91a2f167ee349cec6d6e3c4db1abb00949ce2523d61074442a389fffadb69a3030b85c5168be94f968" alt="watcha" height="200" width="300">
+          :to="{ name: 'recommendOtt', params: {ottId: 97} }"><img src="https://w.namu.la/s/806795f01bfdb99ee07ef5ed4d26c7a76ce99bddf229f51418d040f4387944e4e06d19bcbd84da2a179de6273e3ca30f084aabc97154fb91a2f167ee349cec6d6e3c4db1abb00949ce2523d61074442a75f6771cbc51e0a7d5ba92dd6a8e16fa" alt="watcha" height="200" width="300">
           </router-link>
         </figure>
         <figure class="gallery_item item3"><router-link 
-          :to="{ name: 'recommendOtt', params: {ottId: 337} }"><img src="https://w.namu.la/s/e53fcef546cc1b3f510694f331690215c764c0672c35dd34d207f29ed51c38de36e9f57e5bdd2ee05df77b4c1731d4ce840d3990860b959d27dc073339f94f0705d5bb2a4295a17b8911c5f6d31384752c47dedbd5d7f51bfec4db459a7e6f4b" alt="disney plus" height="200" width="300">
+          :to="{ name: 'recommendOtt', params: {ottId: 337} }"><img src="https://w.namu.la/s/e53fcef546cc1b3f510694f331690215c764c0672c35dd34d207f29ed51c38de36e9f57e5bdd2ee05df77b4c1731d4ce840d3990860b959d27dc073339f94f0705d5bb2a4295a17b8911c5f6d3138475d5877101aa71fbe0fa9a7975f2be49f3" alt="disney plus" height="200" width="300">
           </router-link>
         </figure>
       </div>
